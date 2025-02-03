1417f63d0e409990a6839768238310e9fe8ce89d
Khushboo Jain <khushiejain92@gmail.com>
2025-02-03 13:44:11 +0530

    made improvements, used html form in template

--- oauth/views.py
+++ oauth/views.py
@@ -1,59 +1,47 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib import messages
-from django import forms
-from .models import Userprofile
+from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 class HomeView(View):
     def get(self, request):
         return render(request, 'home.html')
 
-class SignupForm(forms.Form):
-    email = forms.EmailField()
-    password1 = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput())
-
 class SignupView(View):
     def get(self, request):
         if request.user.is_authenticated:
             return redirect('home')
-        return render(request, 'signup.html', {'form': SignupForm()})
+        return render(request, 'signup.html')
 
     def post(self, request):
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            password1 = form.cleaned_data['password1']
-            password2 = form.cleaned_data['password2']
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
 
-            if User.objects.filter(email=email).exists():
-                messages.error(request, "Email already exists")
-                return redirect('signup')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists")
+            return redirect('signup')
 
-            if password1 != password2:
-                messages.error(request, "Passwords don't match")
-                return redirect('signup')
+        if password1 != password2:
+            messages.error(request, "Passwords don't match")
+            return redirect('signup')
 
-            try:
-                validate_password(password1)
-            except ValidationError as e:
-                messages.error(request, e.messages[0])
-                return redirect('signup')
+        try:
+            validate_password(password1)
+        except ValidationError as e:
+            messages.error(request, e.messages[0])
+            return redirect('signup')
 
-            user = User.objects.create_user(
+        user = User.objects.create_user(
             username=email,
             email=email,
             password=password1
-            )
-            login(request, user)
-            return redirect('home')
-
-        return render(request, 'signup.html', {'form': form})
+        )
+        auth_login(request, user)
+        return redirect('home')
 
 class LoginView(View):
     def get(self, request):
