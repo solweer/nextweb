f59c410ac9c270544c049d8267fc42205f276018
Varad J <varad.gcs@gmail.com>
2025-03-02 22:10:01 +0530

    Update redirect urls

--- oauth/config.py
+++ oauth/config.py
@@ -6,7 +6,7 @@ OAUTH_CONFIG = {
         "client_secret": secret.LINKEDIN_CLIENT_SECRET,
         "authorization_url": "https://www.linkedin.com/oauth/v2/authorization",
         "token_url": "https://www.linkedin.com/oauth/v2/accessToken",
-        "redirect_uri": "http://127.0.0.1:8000/linkedin/callback/",  
+        "redirect_uri": secret.LINKEDIN_REDIRECT_URI,
         "callback_url": secret.LINKEDIN_REDIRECT_URI,
         "scopes": [
             "openid",               
