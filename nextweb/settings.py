668c171e014cfab56fcbb92000b09b39916c90ba
Varad J <varad.gcs@gmail.com>
2025-03-02 20:52:28 +0530

    Ready for deploy

--- nextweb/settings.py
+++ nextweb/settings.py
@@ -99,13 +99,22 @@ WSGI_APPLICATION = 'nextweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Add these at the top of your settings.py
+from nextweb.secret import DATABASE_URL
+from urllib.parse import urlparse
+
+tmpPostgres = urlparse(DATABASE_URL)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
